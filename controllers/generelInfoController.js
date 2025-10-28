707f1e7552684c75439b6346b16cb9e6412017cc
ejler <ejler94@live.dk>
2025-10-28 19:40:22 +0100

    css.styles added

--- controllers/generelInfoController.js
+++ controllers/generelInfoController.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import cors from 'cors';
 
 dotenv.config();
 
 const myName = process.env.NAME;
 
+
+//function for personal welcome to main page
 const welcomeName = (req, res, next => {
     const name = req.query.name || req.body.name;
 
@@ -13,3 +17,6 @@ const welcomeName = (req, res, next => {
 
     res.send(`Hello ${name}, welcome to ${myName}'s portfolio!`);
 });
+
+
+// function for contact method to reach out 
