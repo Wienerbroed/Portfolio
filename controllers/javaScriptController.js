636b970b77a04bada0a0d42de76566714d116d02
ejler <ejler94@live.dk>
2025-10-29 17:24:40 +0100

    javaScript page created

--- controllers/javaScriptController.js
+++ controllers/javaScriptController.js
@@ -1,21 +1,20 @@
 
-let stable = array(10);
+let stable = Array(10).fill(null);
 
 
-const sellHorse = (req, res, next => {
+const sellHorse = (req, res, next) => {
     if (stable.length > 0 ) {
         stable.pop();
         res.json(stable);
     } else {
         res.status(400).send('All horses sold');
     }
-});
+};
 
-const buyNewHorses = (req, res, next => {
-    stable.fill('Horses');
-    res.json(stable);
-    res.send('New horses arrived');
-});
+const buyNewHorses = (req, res) => {
+  stable = new Array(10).fill('Horses'); 
+  res.json(stable); 
+};
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { workExperience } from '../controllers/generalInfoController.js';
+import { buyNewHorses, sellHorse } from '../controllers/javaScriptController.js';
 
 
 const router = express.Router();
 
 router.get('/', workExperience);
+router.get('/buy-horses', buyNewHorses);
+router.get('/sell-horse', sellHorse);
 
 export default router;
